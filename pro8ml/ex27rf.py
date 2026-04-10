28ffa2f81fba25aa06f4aedf1dc5d3cc1ab0ba74
sangwoo <asseo1@naver.com>
2026-04-10 18:03:05 +0900

    Day47

--- pro8ml/ex27rf.py
+++ pro8ml/ex27rf.py
@@ -64,6 +64,63 @@ print('분류 정확도 : ', accuracy_score(test_y, pred))
 # 분류 정확도 :  0.827906976744186
 
 
+# 교차 검증 (KFold)
+cross_vali = cross_val_score(model, df_x, df_y, cv=5)
+print(cross_vali)   # [0.75524476 0.8041958  0.81818182 0.83216783 0.83098592]
+print('교차 검증 평균 정확도 : ', np.round(np.mean(cross_vali), 5)) # 0.80816
 
+print('중요 변수 확인하기 ---')
+print('특성(변수) 중요도 : ', model.feature_importances_)   # [0.16172779 0.49842824 0.33984396]
+# feature_importances_ : 각 특성이 예측에 기여한 정도(중요도)를 수치로 표현
+# 값의 합은 1.0, 수치가 클수록 해당 변수가 불순도 감소에 더 많이 기여함.
+
+# 시각화
+import matplotlib.pyplot as plt
+n_features = df_x.shape[1]
+plt.barh(range(n_features), model.feature_importances_, align='center')
+plt.xlabel('Feature importance Score')
+plt.ylabel('Features')
+plt.yticks(np.arange(n_features), df_x.columns)
+plt.ylim(-1, n_features)
+plt.show()
+plt.close()
+
+print()
+# 전체 변수 대상으로 중요도 확인
+# Name, Ticket, Cabin : 문자형 - 바로 사용 불가 (Encoding 필요)
+# PassengerId, Name : Survived와 상관없는 변수
+
+df_imsi = df[['Pclass', 'Age', 'Sex', 'Fare', 'SibSp', 'Parch']]
+
+# Sex 다시 인코딩 필요
+df_imsi.loc[:, 'Sex'] = encoder.fit_transform(df_imsi['Sex'])
+
+train_x, test_x, train_y, test_y = train_test_split(df_imsi, df_y, test_size=0.3, random_state=12)
+
+model.fit(train_x, train_y)
+
+importances = model.feature_importances_
+# 컬럼명 + 중요도
+feature_df = pd.DataFrame({
+    'feature': df_imsi.columns,
+    'importance': importances
+}).sort_values(by='importance', ascending=False)
+print(feature_df)
+#   feature  importance
+# 1     Age    0.292984
+# 3    Fare    0.276886
+# 2     Sex    0.241788
+# 0  Pclass    0.090802
+# 4   SibSp    0.057529
+# 5   Parch    0.040012
+
+# 시각화
+import seaborn as sns
+plt.figure(figsize=(8, 5))
+sns.barplot(x='importance', y='feature', data=feature_df, orient='h')
+plt.xlabel('Feature importance Score')
+plt.ylabel('Feature')
+plt.tight_layout()
+plt.show()
 
 
