533a54763cf61ce033a5936d8f67ba9cf7232737
sangwoo <asseo1@naver.com>
2026-04-23 18:19:32 +0900

    Day55

--- test.py
+++ test.py
@@ -1,21 +1,17 @@
-def solution(nickname):
-    answer = ""
-    for letter in nickname:
-        if letter == "l":
-            answer += "I"
-        elif letter == "w":
-            answer += "vv"
-        elif letter == "W":
-            answer += "VV"
-        elif letter == "O":
-            answer += "0"
-        else:
-            answer += letter
-        print(answer)
-    while len(answer) < 4:
-        answer += "o"
-    if len(answer) > 8:
-        answer = answer[:8]
-    return answer
+# RandomForest는 분류, 회귀 모두 가능. sklearn 모듈은 대개 그러하다.
+# 캘리포니아 주택 가격 데이터로 회귀분석
 
-print(solution("GO"))
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import koreanize_matplotlib
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import r2_score, mean_squared_error
+
+df = pd.read_csv('bike_dataset.csv')
+
+print(df.head(3))
+
+
+
